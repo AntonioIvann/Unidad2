5d7c4d274cf6353d6907c211a20360ddbbf8cfba
Emanuel <maneledcruz15@gmail.com>
2025-02-16 22:14:56 -0600

    Unidad 2 Emanuel Enriquez

--- EnriquezDeLaCruzEmanuel/NumerosPares2.java
+++ EnriquezDeLaCruzEmanuel/NumerosPares2.java
@@ -1,6 +1,6 @@
 class NumerosPares2 {// Nombre de la clase
     public static void main(String[] args) {// Metodo main
-        for (int i = 0; i <= 100; i += 2) {// Usamos el bucle for para recorrer los numeros del 0 al 100
+        for (int i = 0; i <= 100; i++) {// Usamos el bucle for para recorrer los numeros del 0 al 100
             if (i % 2 == 0) {// La condicion if hace que verifique si los numeros son par
                 System.out.println(i);// Imprime los numeros pares del 0 al 100
             }
